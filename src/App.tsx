6afca4b5bdb0ad28c8596a8a01484f01f5fd7ce4
Tibor Fazekas <tibcso0322@gmail.com>
2022-09-26 10:32:58 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+
+function App(): JSX.Element {
+
+    return <>
+        <div className='relative h-screen w-screen'>
+          
+        </div>
+    </>;
+
+}
+
+export default App;
